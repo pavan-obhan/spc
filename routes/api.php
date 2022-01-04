3e953c86d6daec29fd92147b206e549d72bc2c53
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-04 18:43:13 +0530

    inspection definition show api create

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\InspectionController;
+use App\Http\Controllers\InspectionDefinitionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('inspection_definition',[InspectionDefinitionController::class,'show']);
+Route::put('inspection_item/create',[InspectionDefinitionController::class,'store']);
+Route::put('inspection_item/update',[InspectionDefinitionController::class,'store']);
+//Route::get('inspection',[InspectionController::class,'show']);
