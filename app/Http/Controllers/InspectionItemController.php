0736335804ef770fabd6b98f26010193811b9f0e
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-05 13:05:22 +0530

    Add InspectionController

---
+++ app/Http/Controllers/InspectionItemController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class InspectionItemController extends Controller
+{
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+}
