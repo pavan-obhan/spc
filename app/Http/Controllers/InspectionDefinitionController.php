8aa97ecfaf9da9858a42900e4d5370dd2c81d36e
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-07 00:09:00 +0530

    add export method

--- app/Http/Controllers/InspectionDefinitionController.php
+++ app/Http/Controllers/InspectionDefinitionController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 //use App\Models\InspectionDefinition;
+use App\Exports\InspectionExport;
 use App\Models\InspectionDefinition;
+use Maatwebsite\Excel\Facades\Excel;
 
 class InspectionDefinitionController extends Controller
 {
     public function index()
     {
         return InspectionDefinition::with('inspection_definition_item', 'inspection_definition_group')->get();
+    }
+
+    public function export()
+    {
+        return Excel::download(new InspectionExport, 'inspection.xlsx');
     }
 
 }
