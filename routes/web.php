25149f9afdd2d0c9a10a73eb21397b269a4968fb
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-07 00:12:39 +0530

    Update project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('inspection/export',[\App\Http\Controllers\InspectionDefinitionController::class,'export']);
+
