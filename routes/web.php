5c4a1f9dd3ad251c6dd45a992d8d282a850bd66e
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-11 13:23:56 +0530

    Refactor Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InspectionDefinitionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('inspection/export',[\App\Http\Controllers\InspectionDefinitionController::class,'export']);
+Route::get('inspection/export',[InspectionDefinitionController::class,'export']);
 
