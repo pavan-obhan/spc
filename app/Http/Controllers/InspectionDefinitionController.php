afc507ecfc0551c4a6a2426fe8cfbb930b1d7dec
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-11 13:22:06 +0530

    Add has many relationship

--- app/Http/Controllers/InspectionDefinitionController.php
+++ app/Http/Controllers/InspectionDefinitionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 //use App\Models\InspectionDefinition;
 use App\Exports\InspectionExport;
+use App\Exports\ItemMultiSheetExport;
 use App\Models\InspectionDefinition;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -15,7 +16,8 @@ class InspectionDefinitionController extends Controller
 
     public function export()
     {
-        return Excel::download(new InspectionExport, 'inspection.xlsx');
+
+        return Excel::download(new ItemMultiSheetExport(2021), 'inspection.xlsx');
     }
 
 }
