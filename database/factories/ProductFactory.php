4894b75955212e085fbc1550cdb2aec6299e042b
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-11 13:24:51 +0530

    Ignore factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -14,7 +14,7 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            'name'=>$this->faker->word()
+            'name'=>$this->faker->monthName()
         ];
     }
 }
