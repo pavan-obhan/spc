a5b3ea14e4e51dcb4aaa1d8a78ae78f596f1983e
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-12 18:42:37 +0530

    For new values add if not exist, if exist use id

--- app/Imports/InspectionDefinitionItemsImport.php
+++ app/Imports/InspectionDefinitionItemsImport.php
@@ -6,6 +6,7 @@ use App\Models\InspectionDefinition;
 use App\Models\InspectionDefinitionGroup;
 use App\Models\InspectionDefinitionItem;
 use App\Models\MeasuringTool;
+use App\Models\Product;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\SkipsErrors;
@@ -24,13 +25,43 @@ class InspectionDefinitionItemsImport implements ToCollection, WithHeadingRow, S
     {
         foreach ($rows as $row) {
             $item = new InspectionDefinitionItem();
-            $item->inspection_definition_id
-                = InspectionDefinition::where('description', $row['description'])->first()->id;
+
+
+            if (!InspectionDefinition::where('description', $row['description'])->first()) {
+                if (!Product::where('name', $row['description'])->first()) {
+                    $product = new Product();
+                    $product->name = $row['description'];
+                    $product->save();
+                }
+                $definition = new InspectionDefinition();
+                $definition->description = $row['description'];
+                $definition->product_id
+                    = Product::where('name', $row['description'])->first()->id;
+                $definition->save();
+            }
+            $item->inspection_definition_id = InspectionDefinition::where('description', $row['description'])->first()->id;
+
+            if (!InspectionDefinitionGroup::where('name', $row['group'])->first()) {
+                $group = new InspectionDefinitionGroup();
+                $group->name = $row['group'];
+                $group->inspection_definition_id = InspectionDefinition::where('description', $row['description'])->first()->id;
+                $group->save();
+            }
             $item->inspection_definition_group_id
                 = InspectionDefinitionGroup::where('name', $row['group'])->first()->id;
+
+
             $item->reference_number = $row['reference_number'];
+
+            if (!MeasuringTool::where('name',$row['measuring_tool'])->first()){
+                $tool = new MeasuringTool();
+                $tool->name = $row['measuring_tool'];
+                $tool->type = $row['value'];
+                $tool->save();
+            }
             $item->measuring_tool_id
-                = MeasuringTool::where('name',$row['measuring_tool'])->first()->id;
+                = MeasuringTool::where('name', $row['measuring_tool'])->first()->id;
+
             $item->serial_number = $row['serial_number'];
             $item->specification = $row['specification'];
             $item->created_at = $row['date'];
