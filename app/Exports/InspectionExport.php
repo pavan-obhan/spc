f087a1bb1a361a2cd3389209336c747840cef002
PavanObhan <pavan.obhan@noovosoft.com>
2022-01-12 15:35:31 +0530

    add rows to inspection export

--- app/Exports/InspectionExport.php
+++ app/Exports/InspectionExport.php
@@ -10,7 +10,8 @@ use Maatwebsite\Excel\Concerns\WithTitle;
 
 class InspectionExport implements FromCollection, ShouldAutoSize, WithHeadings, WithTitle
 {
-    private $year,$month;
+    private $year, $month;
+
     public function __construct(int $year, int $month)
     {
         $this->year = $year;
@@ -20,23 +21,26 @@ class InspectionExport implements FromCollection, ShouldAutoSize, WithHeadings, 
     public function collection()
     {
         $items = InspectionDefinitionItem::query()
-            ->whereYear('created_at',$this->year)
-            ->whereMonth('created_at',$this->month)->get();
+            ->whereYear('created_at', $this->year)
+            ->whereMonth('created_at', $this->month)->get();
 
-        $table = $items->map(function ($item){
-           return[
-               $item->inspectionDefinitionGroup->name,
-               $item->reference_number,
-               $item->measuring_tool->name,
-               $item->serial_number,
-               $item->specification,
-               $item->created_at->format('d.m.Y'),
-               $item->created_at->format('H:i:s'),
-               $item->inspectionDefinition->inspection->pluck('fixture_number')->implode(','),
-               $item->inspection_item->pluck('value')->implode(' ')
-           ];
+        $table = $items->map(function ($item) {
+            return [
+                $item->inspectionDefinition->description?$item->inspectionDefinition->description:"No Description",
+                $item->inspectionDefinitionGroup->name,
+                $item->reference_number,
+                $item->measuring_tool->name,
+                $item->serial_number,
+                $item->specification,
+                $item->created_at->format('d.m.Y'),
+                $item->created_at->format('H:i:s'),
+                $item->upper_specification_limit,
+                $item->lower_specification_limit,
+                $item->spc_enabled,
+                $item->inspectionDefinition->inspection->pluck('fixture_number')->implode(','),
+                $item->inspection_item->pluck('value')->implode(' ')
+            ];
         });
-
 
         return collect([$table]);
     }
@@ -47,6 +51,7 @@ class InspectionExport implements FromCollection, ShouldAutoSize, WithHeadings, 
     {
         return [
             [
+                'Description',
                 'Group',
                 'Reference Number',
                 'Measuring Tool',
@@ -54,12 +59,17 @@ class InspectionExport implements FromCollection, ShouldAutoSize, WithHeadings, 
                 'Specification',
                 'Date',
                 'Time',
+                'Upper Specific Limit',
+                'Lower Specific Limit',
+                'SPC Enabled',
+                'Fixture Number',
                 'Value'
             ],
         ];
     }
+
     public function title(): string
     {
-        return \DateTime::createFromFormat('!m',$this->month)->format('F');
+        return \DateTime::createFromFormat('!m', $this->month)->format('F');
     }
 }
